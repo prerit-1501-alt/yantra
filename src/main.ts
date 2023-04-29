2ca7158fd77bbcea99693849d2151b03de5534dd
prerit__singh <prerit.singh@insurancedekho.com>
2023-04-29 20:06:45 +0530

    changed default port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 10000;
+  await app.listen(PORT);
 }
 bootstrap();
